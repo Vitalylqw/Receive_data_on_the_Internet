099adc8de6ee0bac407d59132f5c007cec34f961
Vitalylqw <vitalylqw@gmail.com>
2020-06-04 22:12:36 +0300

    ok

--- 7._Selenium _in_Python/mvideo_pars_selenium.py
+++ 7._Selenium _in_Python/mvideo_pars_selenium.py
@@ -13,18 +13,34 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pendulum
 from pprint import pprint
+import json;
 
 list_leters=[]
 driver=webdriver.Chrome()
 driver.get('https://www.mvideo.ru/')
-time.sleep(2)
+time.sleep(10)
+pages=1
 while True:
     try:
-        button = WebDriverWait(driver,10).until(ec.presence_of_element_located((By.CLASS_NAME,'special-offers__more-btn')))
-        print(pages)
+        time.sleep(5)
+        # button = driver.find_elements_by_xpath("//a[@class='next-btn sel-hits-button-next']")
+        button = WebDriverWait(driver,10).until(ec.presence_of_element_located((By.XPATH,'//div[@data-init="ajax-category-carousel"][2]//a[@class="next-btn sel-hits-button-next"]')))
         # button = driver.find_element_by_class_name('special-offers__more-btn')
         button.click()
         pages+=1
-    except:
-        print(f'всего страниц {pages}')
+    except Exception as e:
+        print(e,f'всего страниц {pages}')
         break
+
+bloks= driver.find_elements_by_xpath("//div[@data-sel='new_cart-carousel-gallery_accessories']")
+goods=bloks[1].find_elements_by_xpath(".//li[@class='gallery-list-item height-ready']//a[@data-product-info]")
+
+
+client = MongoClient('localhost:27017')
+db = client.goods
+collection = db.mvideo
+collection.drop()
+for i in goods:
+    collection.insert_one(json.loads(i.get_attribute('data-product-info')))
+
+
